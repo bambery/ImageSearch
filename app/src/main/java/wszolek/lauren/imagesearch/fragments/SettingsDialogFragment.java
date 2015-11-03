72171ccdffaef9570370dd9141a559344c576a1a
Lauren Wszolek <lwszolek@gmail.com>
2015-11-02 23:06:39 -0800

    definitely need to figure out how to properly pass back the filter data

--- app/src/main/java/wszolek/lauren/imagesearch/fragments/SettingsDialogFragment.java
+++ app/src/main/java/wszolek/lauren/imagesearch/fragments/SettingsDialogFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,7 +15,7 @@ import wszolek.lauren.imagesearch.R;
 import wszolek.lauren.imagesearch.models.SearchFilters;
 
 
-public class SettingsDialogFragment extends DialogFragment{
+public class SettingsDialogFragment extends DialogFragment {
 
     // http://developer.android.com/reference/android/app/AlertDialog.Builder.html
     // setting spinner values: http://stackoverflow.com/questions/11072576/set-selected-item-of-spinner-programmatically
@@ -25,7 +26,7 @@ public class SettingsDialogFragment extends DialogFragment{
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View filtersView = inflater.inflate(R.layout.activity_settings, null);
-        SearchFilters sFilters = getArguments().getParcelable("filters");
+        final SearchFilters sFilters = getArguments().getParcelable("filters");
         builder.setView(filtersView);
 
         //populate the size spinner
@@ -34,6 +35,17 @@ public class SettingsDialogFragment extends DialogFragment{
         sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sizeSpinner.setAdapter(sizeAdapter);
         sizeSpinner.setSelection(sizeAdapter.getPosition(sFilters.getImageSize()));
+        sizeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sFilters.setImageSize(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // don't do anything
+            }
+        });
 
         //populate the color spinner
         Spinner colorSpinner = (Spinner) filtersView.findViewById(R.id.spColors);
@@ -52,6 +64,7 @@ public class SettingsDialogFragment extends DialogFragment{
                 .setNegativeButton(R.string.clear, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // clear all values set on filter
+                        // not working
                     }
                 });
         // Create the AlertDialog object and return it
